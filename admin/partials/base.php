1240d0011eba17e32879b74640b66520e0503a9b
ri <example@gmail.com>
2025-04-05 00:42:13 +0800

    l

--- admin/partials/base.php
+++ admin/partials/base.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+SESSION_START();
 require_once '../SalesInventorySystem/auth/session_check.php';
 
 ?>
@@ -11,6 +11,9 @@ require_once '../SalesInventorySystem/auth/session_check.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
